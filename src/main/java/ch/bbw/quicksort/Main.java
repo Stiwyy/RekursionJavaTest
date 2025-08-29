23cfd782ecda99df1291ed1acd0d72ecc276f4f8
Andrin <andrinrenggli@outlook.com>
2025-08-29 11:23:18 +0200

    implement quicksort

--- src/main/java/ch/bbw/quicksort/Main.java
+++ src/main/java/ch/bbw/quicksort/Main.java
@@ -1,17 +1,18 @@
 package ch.bbw.quicksort;
-
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
+import ch.bbw.quicksort.Quicksort;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> list = new ArrayList<>();
-        list.add(3);
-        list.add(6);
-        list.add(8);
-        list.add(10);
-        list.add(1);
-        list.add(2);
+        int[] arr = {5, 9, 4, 6, 5, 3};
 
+        System.out.println("Unsortiertes Array:");
+        System.out.println(Arrays.toString(arr));
+
+        Quicksort sorter = new Quicksort();
+        sorter.quickSort(arr, 0, arr.length - 1);
+
+        System.out.println("Sortiertes Array:");
+        System.out.println(Arrays.toString(arr));
     }
 }
