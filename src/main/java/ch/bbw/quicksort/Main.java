c785c5924fcdadb740a3de9c50c3b5a62305f075
Andrin <andrinrenggli@outlook.com>
2025-08-29 11:06:00 +0200

    prepare for quick sort implementation

---
+++ src/main/java/ch/bbw/quicksort/Main.java
@@ -0,0 +1,17 @@
+package ch.bbw.quicksort;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(3);
+        list.add(6);
+        list.add(8);
+        list.add(10);
+        list.add(1);
+        list.add(2);
+
+    }
+}
---
+++ src/main/java/ch/bbw/quicksort/Quicksort.java
@@ -0,0 +1,4 @@
+package ch.bbw.quicksort;
+
+public class Quicksort {
+}
